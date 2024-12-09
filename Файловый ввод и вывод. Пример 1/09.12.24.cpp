209d84afc85cd3d9b912524b49d5ca3e24f7cee2
chebanoleksandra <alexa30che@gmail.com>
2024-12-09 16:12:02 +0200

    done

--- Файловый ввод и вывод. Пример 1/09.12.24.cpp
+++ Файловый ввод и вывод. Пример 1/09.12.24.cpp
@@ -2,9 +2,54 @@
 #include <fstream>
 using namespace std;
 
+class Student
+{
+	string name;
+	string surname;
+	int age;
+public:
+	Student() = default;
+	Student(const char* n, const char* s, int a)
+	{
+		name = n;
+		surname = s;
+		age = a;
+	}
+	void Print()
+	{
+		cout << "Name: " << name << "\nSurname: " << surname << "\nAge: " << age << endl;
+	}
+	void SaveFile()
+	{
+		ofstream write;
+		write.open("StudentFile.txt", ios::out | ios::app);
+		write << name << " " << surname << " " << age << " y. o.\n";
+		write.close();
+	}
+	void ReadFile()
+	{
+		ifstream read("StudentFile.txt");
+		if (read)
+		{
+			const int size = 100;
+			do
+			{
+				char buff[size] = {};
+				read.getline(buff, size);
+				cout << buff << endl;
+			} while (read);
+			read.close();
+		}
+		else
+		{
+			cout << "Could not open the file" << endl;
+		}
+	}
+};
+
 int main()
 {
-	char str[100];
+	/*char str[100];
 	cout << "Enter text:\n";
 	cin.getline(str, 100);
 
@@ -19,5 +64,14 @@ int main()
 		read >> buff;
 		cout << buff << " ";
 	}
-	read.close();
+	read.close();*/
+
+	Student sasha("Oleksandra", "Cheban", 16);
+	sasha.Print();
+	sasha.SaveFile();
+
+	Student sofia("Sofia", "Cheban", 12);
+	sofia.SaveFile();
+
+	sofia.ReadFile();
 }
